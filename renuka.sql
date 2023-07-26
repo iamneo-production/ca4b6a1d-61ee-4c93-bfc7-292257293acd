dc95276f5e8e75437e0f89f762abc5b3de7e8199
RenukaPachipulusu <pachipulusurenuka@gmail.com>
2023-07-26 09:17:59 +0000

    Backup Commit

---
+++ renuka.sql
@@ -0,0 +1,27 @@
+
+SELECT COUNT(*) AS TotalUsers
+FROM FACEBOOK
+WHERE DOB_YEAR = 1999;
+
+
+SELECT AGE, COUNT(*) AS UserCount
+FROM FACEBOOK
+GROUP BY AGE;
+
+
+SELECT COUNT(*) AS TotalUsers
+FROM facebook
+WHERE AGE > 15;
+
+
+SELECT (COUNT(*) * 100.0) / (SELECT COUNT(*) FROM facebook) AS FemalePercentage
+FROM facebook
+WHERE GENDER = 'female'
+GROUP BY GENDER;
+
+
+SELECT (COUNT(*) * 100.0) / (SELECT COUNT(*) FROM facebook ) AS FemalePercentage
+FROM facebook
+WHERE AGE > 17 AND GENDER = 'female'
+GROUP BY GENDER;
+

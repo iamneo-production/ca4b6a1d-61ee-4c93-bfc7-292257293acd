9196171976657027080674a755ae4db608937364
nagasai156 <nagasaichimmili156@gmail.com>
2023-07-29 07:58:02 +0000

    final

---
+++ Roopesh_Nallakukkala.sql
@@ -0,0 +1,30 @@
+SELECT COUNT(*) AS TotalUsers
+FROM facebook
+WHERE DOB_YEAR = 1999;
+
+
+
+SELECT AGE, COUNT(*) AS UserCount
+FROM facebook
+GROUP BY AGE
+order by AGE;
+
+
+
+SELECT COUNT(*) AS TotalUsers
+FROM facebook
+WHERE AGE > 15;
+
+
+
+SELECT (COUNT(*) * 100.0) / (SELECT COUNT(*) FROM facebook ) AS FemalePercentage
+FROM facebook
+WHERE GENDER = 'female'
+group by gender;
+
+
+
+SELECT (COUNT(*) * 100.0) / (SELECT COUNT(*) FROM facebook where age>17) AS FemalePercentage
+FROM facebook
+WHERE AGE > 17 AND GENDER = 'female'
+GROUP BY GENDER;    

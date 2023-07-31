d46b405ec404c49c3bcb4bac0863309cc792673e
Harika1115 <namburuharika@gmail.com>
2023-07-31 17:15:39 +0000

    sql file from harika

--- -- HarikaNamburu.sql
+++ -- HarikaNamburu.sql
@@ -1,5 +1,3 @@
-
-
 -- query to find the total number of users who were born in the year 1999.
 select count(*) AS total_users from facebook
  where dob_year=1999;
@@ -7,15 +5,15 @@ select count(*) AS total_users from facebook
 -- query to count the number of social media users for each age group.
  select age, count(*) as age_count from FACEBOOK
   group by age 
-  order by age;
+  order by age asc;
 
 --query to find total users in social media above age 15.
- select count(*) users_above15 from FACEBOOK 
- where age>15;
+ select count(*) as Users_above15 from FACEBOOK  
+  where Age>15;
 
 -- query to find the percentage of female users who are using social media.
 select count(*)*100/ (select count(*) from facebook) AS female_percentage 
-from FACEBOOK 
+ from FACEBOOK 
 where gender='female' 
 group by gender;
 

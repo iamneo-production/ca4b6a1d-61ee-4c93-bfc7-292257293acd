5303b5e7a7c2e3c79985d9126aaa1f1a60582fac
Thanu0918 <naiduthanuja4@gmail.com>
2023-07-31 13:02:54 +0000

    COPY

--- THANUJA_Naidu.sql
+++ THANUJA_Naidu.sql
@@ -1,11 +1,17 @@
 -- Query that Find the total number of users who were born in the year 1999.
  Select count(*) as Total_users froM Facebook where Dob_YeaR = 1999;
 
+
+
 -- Query that Count the number of social media users for each age group.`
  select Age, count(*) as Users_count from FACEBOOK Group BY AgE ORDER by AgE ASC;
 
+
+
  --Query that Find total users in Social Media above age 15.
  Select count(*) AS count_users from FACeBOOK where AgE>15;
+
+
 
  -- Query that Find the percentage of Female users who are using Social media.
  Select count(*)*100/ (select counT(*) from facebook) AS Percentage From FACEbOOK Where GendeR='female' Group By GendeR;
